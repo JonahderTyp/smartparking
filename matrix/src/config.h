fd18760745abc0f51a82b22d81a8f61dbfb9eecd
Jonah Wille <jonah.wille10@gmail.com>
2024-11-14 21:12:43 +0100

    Add initial project structure with configuration, source files, and documentation

---
+++ matrix/src/config.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#define R0 A0
+#define R1 A1
+#define R2 A2
+#define R3 A3
+#define R4 2
+#define R5 3
+#define R6 4
+#define R7 5
+
+#define C0 6
+#define C1 7
+#define C2 8
+#define C3 9
+#define C4 10
+#define C5 11
+#define C6 12
+#define C7 13
---
+++ matrix/src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+
+#include "config.h"
+
+uint8_t rows[] = {R0, R1, R2, R3, R4, R5, R6, R7};
+uint8_t cols[] = {C0, C1, C2, C3, C4, C5, C6, C7};
+
+void activate_row(uint8_t row) { digitalWrite(rows[row], HIGH); }
+
+void deactivate_row(uint8_t row) { digitalWrite(rows[row], LOW); }
+
+void activate_col(uint8_t col) { digitalWrite(cols[col], LOW); }
+
+void deactivate_col(uint8_t col) { digitalWrite(cols[col], HIGH); }
+
+void display_matrix(bool matrix[8][8], uint16_t loops = 64) {
+  for (uint16_t i = 0; i < loops; i++) {
+    for (uint8_t row = 0; row < 8; row++) {
+      activate_row(row);
+      for (uint8_t col = 0; col < 8; col++) {
+        if (matrix[row][col])
+          activate_col(col);
+        else
+          deactivate_col(col);
+        _delay_us(1);
+      }
+      delay(2);
+      for (uint8_t col = 0; col < 8; col++) deactivate_col(col);
+      deactivate_row(row);
+    }
+  }
+}
+
+bool arrow_right_down[8][8] = {
+    {1, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 1}, {0, 0, 0, 0, 0, 1, 0, 1},
+    {0, 0, 0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 1, 1, 1, 1}};
+
+bool arrow_left_down[8][8] = {
+    {0, 0, 0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0},
+    {1, 0, 0, 1, 0, 0, 0, 0}, {1, 0, 1, 0, 0, 0, 0, 0},
+    {1, 1, 0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 0, 0, 0, 0}};
+
+bool cross[8][8] = {{1, 0, 0, 0, 0, 0, 0, 1}, {0, 1, 0, 0, 0, 0, 1, 0},
+                    {0, 0, 1, 0, 0, 1, 0, 0}, {0, 0, 0, 1, 1, 0, 0, 0},
+                    {0, 0, 0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 1, 0}, {1, 0, 0, 0, 0, 0, 0, 1}};
+
+bool fill[8][8] = {{1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1},
+                   {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1},
+                   {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1},
+                   {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}};
+
+void setup() {
+  for (auto row : rows) {
+    pinMode(row, OUTPUT);
+    digitalWrite(row, HIGH);
+  }
+
+  for (auto col : cols) {
+    pinMode(col, OUTPUT);
+    digitalWrite(col, LOW);
+  }
+}
+
+void loop() {
+  display_matrix(arrow_left_down);
+  delay(1000);
+}
