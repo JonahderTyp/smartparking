45086fd4284877b02f10630f5a1d839df3b8791a
Jonah Wille <jonah.wille10@gmail.com>
2024-11-15 10:00:20 +0100

    Refactor CoAP client implementation and add ultrasonic sensor handling

---
+++ parkhaus/src/config.h
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+const char* coap_server_str = "jonahwille.de";
+
+const int coap_port = 5683;
--- parkhaus/src/main.cpp
+++ parkhaus/src/main.cpp
@@ -1,37 +1,97 @@
+#include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
+#include <Ultrasonic.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
+#include <coap-simple.h>
 
-#include "Thing.CoAP.h"
-#include "Thing.CoAP/Client.h"
-#include "Thing.CoAP/ESP/UDPPacketProvider.h"
+#include "config.h"
 
-//[RECOMMENDED] Alternatively, if you are NOT using Arduino IDE you can include
-// each file you need as bellow: #include "Thing.CoAP/Client.h" #include
-// "Thing.CoAP/ESP/UDPPacketProvider.h"
+IPAddress ip;
 
-// Declare our CoAP client and the packet handler
-Thing::CoAP::Client coapClient;
-Thing::CoAP::ESP::UDPPacketProvider udpProvider;
+#define LED_PIN 2
 
-// Change here your WiFi settings
-char* ssid = "YourWiFiSSID";
-char* password = "YourWiFiPassword";
-const char* serverHostname = "yourserver.com";
+Ultrasonic u1(22, 23);
+Ultrasonic u2(24, 25);
+Ultrasonic u3(26, 27);
+Ultrasonic u4(28, 29);
 
-void sendMessage(const char* payload) {
-  // Make a post
-  coapClient.Get("create1", payload, [](Thing::CoAP::Response response) {
-    std::vector<uint8_t> recivedpayload = response.GetPayload();
-    std::string received(recivedpayload.begin(), recivedpayload.end());
-    Serial.println("Server sent the following message:");
-    Serial.println(received.c_str());
-  });
+uint32_t last_millis = 0;
+
+bool p1 = false;
+bool p2 = false;
+bool p3 = false;
+bool p4 = false;
+
+void checkBays() {
+  if (u1.read(CM) < 10) {
+    p1 = true;
+  } else {
+    p1 = false;
+  }
+
+  if (u2.read(CM) < 10) {
+    p2 = true;
+  } else {
+    p2 = false;
+  }
+
+  if (u3.read(CM) < 10) {
+    p3 = true;
+  } else {
+    p3 = false;
+  }
+
+  if (u4.read(CM) < 10) {
+    p4 = true;
+  } else {
+    p4 = false;
+  }
 }
 
+Adafruit_NeoPixel strip(4, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setBayLEDs(bool b1, bool b2, bool b3, bool b4) {
+  static uint32_t used = strip.Color(255, 0, 0);
+  static uint32_t free = strip.Color(0, 255, 0);
+
+  if (b1) {
+    strip.setPixelColor(0, used);
+  } else {
+    strip.setPixelColor(0, free);
+  }
+
+  if (b2) {
+    strip.setPixelColor(1, used);
+  } else {
+    strip.setPixelColor(1, free);
+  }
+
+  if (b3) {
+    strip.setPixelColor(2, used);
+  } else {
+    strip.setPixelColor(2, free);
+  }
+
+  if (b4) {
+    strip.setPixelColor(3, used);
+  } else {
+    strip.setPixelColor(3, free);
+  }
+
+  strip.show();
+}
+
+WiFiUDP udp;
+Coap coap(udp);
+
 void setup() {
-  // Initializing the Serial
   Serial.begin(115200);
   Serial.println("Initializing");
+
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
 
   // Try and wait until a connection to WiFi was made
   WiFi.begin(ssid, password);
@@ -43,14 +103,11 @@ void setup() {
   Serial.println("My IP: ");
   Serial.println(WiFi.localIP());
 
-  // Configure our server to use our packet handler (It will use UDP)
-  coapClient.SetPacketProvider(udpProvider);
-
   // Resolve the IP address of the server using DNS
   IPAddress ip;
-  if (WiFi.hostByName(serverHostname, ip)) {
+  if (WiFi.hostByName(coap_server_str, ip)) {
     Serial.print("Resolved IP for ");
-    Serial.print(serverHostname);
+    Serial.print(coap_server_str);
     Serial.print(": ");
     Serial.println(ip);
   } else {
@@ -59,10 +116,26 @@ void setup() {
   }
 
   // Connect CoAP client to a server
-  coapClient.Start(ip, 5683);
-
-  // Send A Message
-  sendMessage("Test Message");
+  coap.start();
 }
 
-void loop() { coapClient.Process(); }
+void loop() {
+  checkBays();
+  setBayLEDs(p1, p2, p3, p4);
+
+  if (millis() - last_millis > 1000) {
+    last_millis = millis();
+    // Construct payload as a JSON-like string
+    String payload = "{";
+    payload += "\"p1\":" + String(p1 ? "true" : "false") + ",";
+    payload += "\"p2\":" + String(p2 ? "true" : "false") + ",";
+    payload += "\"p3\":" + String(p3 ? "true" : "false") + ",";
+    payload += "\"p4\":" + String(p4 ? "true" : "false");
+    payload += "}";
+
+    // Send data to the CoAP server
+    coap.put(ip, coap_port, "boolean", payload.c_str(), payload.length());
+  }
+
+  coap.loop();  // Keep the CoAP client running
+}
